d7f6b99cc8fa0ce0b87b95d07c1020c3b81cc7dd
tao-z141 <taozaddi@gmail.com>
2025-07-04 09:22:39 +0200

    Update 02_clean_google_trends.py

--- 02_clean_google_trends.py
+++ 02_clean_google_trends.py
@@ -1,9 +1,8 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-final_df = pd.read_csv('data/google_trends_data.csv')  
+final_df = pd.read_csv('raw/google_trends_data.csv')
 
-# Filtrer les données où isPartial est False : isPartial signifie que le scrapping n'as pas réussit à recuperer la données completes du jour
 filtered_df = final_df[final_df['isPartial'] == False]
 
 MYSQL_USER = "root"
@@ -16,16 +15,28 @@ engine = create_engine(
     f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 )
 
-# Calcule l'intérêt moyen par pays et mot-clé
 mean_interest = filtered_df.groupby(['country', 'keyword'])['interest'].mean().reset_index()
 
-# mot-clé le plus populaire par pays
 idx = mean_interest.groupby('country')['interest'].idxmax()
 top_languages = mean_interest.loc[idx].reset_index(drop=True)
 
-top_languages.to_csv('data/languagePopulaireParPays_data.csv', index=False)
-print("Fichier 'languagePopulaireParPays_data.csv' créé avec succès.")
-
 table_name = 'top_languages_by_country'
-top_languages.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
-print(f"Table '{table_name}' insérée avec succès dans la base MySQL '{MYSQL_DATABASE}'.")
+
+with engine.connect() as conn:
+    conn.execute(text(f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            country VARCHAR(255),
+            keyword VARCHAR(255),
+            interest FLOAT,
+            PRIMARY KEY (country)
+        );
+    """))
+
+with engine.connect() as conn:
+    existing_df = pd.read_sql_table(table_name, conn)
+
+merged = top_languages.merge(existing_df, on=['country', 'keyword', 'interest'], how='left', indicator=True)
+new_rows = merged[merged['_merge'] == 'left_only'].drop(columns=['_merge'])
+
+if not new_rows.empty:
+    new_rows.to_sql(name=table_name, con=engine, if_exists='append', index=False)
